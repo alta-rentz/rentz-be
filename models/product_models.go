7b74778812a722bc3b0a40d755409b35c21d154e
alfynf <alfynurf36@gmail.com>
2021-12-17 20:40:28 +0700

    edit lat llon type

--- models/product_models.go
+++ models/product_models.go
@@ -56,8 +56,8 @@ type GetProduct struct {
 	Price            int
 	Description      string
 	Stock            int
-	Longitude        string
-	Latitude         string
+	Longitude        float64
+	Latitude         float64
 	Url              []string
 	Guarantee        []string
 }
